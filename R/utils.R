50810704907c58575df80ab58e86d063164c8ccb
timoast <4591688+timoast@users.noreply.github.com>
2022-11-17 11:44:21 -0500

    Remove duplicated function

--- R/utils.R
+++ R/utils.R
@@ -1205,47 +1205,6 @@ RowMergeSparseMatrices <- function(mat1, mat2) {
   return(new.mat)
 }
 
-#' Update slots in an object
-#'
-#' @param object An object to update
-#'
-#' @return \code{object} with the latest slot definitions
-#'
-#' @importFrom methods slotNames slot
-#'
-#' @export
-#'
-#' @concept utils
-#'
-UpdateSlots <- function(object) {
-  object.list <- sapply(
-    X = slotNames(x = object),
-    FUN = function(x) {
-      return(tryCatch(
-        expr = slot(object = object, name = x),
-        error = function(...) {
-          return(NULL)
-        }
-      ))
-    },
-    simplify = FALSE,
-    USE.NAMES = TRUE
-  )
-  object.list <- Filter(f = Negate(f = is.null), x = object.list)
-  object.list <- c('Class' = class(x = object)[1], object.list)
-  object <- do.call(what = 'new', args = object.list)
-  for (x in setdiff(x = slotNames(x = object), y = names(x = object.list))) {
-    xobj <- slot(object = object, name = x)
-    if (is.vector(x = xobj) && !is.list(x = xobj) && length(x = xobj) == 0) {
-      slot(object = object, name = x) <- vector(
-        mode = class(x = xobj),
-        length = 1L
-      )
-    }
-  }
-  return(object)
-}
-
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Methods for Seurat-defined generics
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
