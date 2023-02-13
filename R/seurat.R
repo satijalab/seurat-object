694a56a57c80b305c945e27f4b4b0b2117f8afae
yuhanH <yh1970@nyu.edu>
2023-02-13 12:20:56 -0500

    updateobject for sct

--- R/seurat.R
+++ R/seurat.R
@@ -965,14 +965,16 @@ UpdateSeuratObject <- function(object) {
       }
       # Update Assays, DimReducs, and Graphs
       for (x in names(x = object)) {
-
         message("Updating slots in ", x)
         xobj <- object[[x]]
         xobj <- suppressWarnings(
           expr = UpdateSlots(object = xobj),
           classes = 'validationWarning'
         )
-        if (x == "SCT"){
+        if (IsSCT(assay = xobj)){
+          if (!inherits(x = xobj, what = "SCTAssay")) {
+            xobj <- as(object = xobj, Class = "SCTAssay")
+          }
           sctmodels <- names(x = slot(object = xobj, name = "SCTModel.list"))
           for (sctmodel in sctmodels){
             median_umi <- tryCatch(
