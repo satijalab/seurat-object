28215664573722cb417b6b82bdd90672cfdb53a6
Gesmira <gesmiramolla@gmail.com>
2023-06-13 11:38:43 -0400

    saveseuratbp

--- R/seurat.R
+++ R/seurat.R
@@ -978,12 +978,15 @@ SaveSeuratBP <- function(
               call. = FALSE, immediate. = TRUE)
       ldat <- LayerData(object[[assay]], layer = layer)
       matrices <- BPCells:::all_matrix_inputs(ldat)
-      for (i in seq_along(matrices)) {
-        if (inherits(x = matrices[[i]], what = "MatrixDir")) {
-          matrices[[i]]@dir <- df[df$layer == layer, ]$path[i]
+      matrix.dirs <- which(sapply(matrices, function(x) inherits(x, "MatrixDir")))
+      if (length(matrix.dirs) == nrow(df[df$layer == layer, ])){
+        for (i in 1:length(matrix.dirs)) {
+          matrices[[matrix.dirs[i]]]@dir <- df[df$layer == layer, ]$path[i]
         }
         BPCells:::all_matrix_inputs(ldat) <- matrices
         LayerData(object[[assay]], layer = layer) <- ldat
+      } else {
+        stop("Directories to replace is not equal to length of directories")
       }
     }
     p()
