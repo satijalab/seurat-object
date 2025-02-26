5b2cc68968a124aa834e97df16804f0bcb83504f
David Collins <dcollins@nygenome.org>
2025-02-26 10:39:29 -0500

    Add HVF helper to test_assay5.R

--- tests/testthat/test_assay5.R
+++ tests/testthat/test_assay5.R
@@ -21,3 +21,44 @@ get_test_assay <- function(ncells, nfeatures, assay_version) {
   
   return(assay)
 }
+
+#' Mocks "highly variable feature" annotations and adds them to the 
+#' feature-level metadata of `assay`.
+add_hvf_info <- function(assay, nfeatures, method_name, layer_name) {
+  variable_features <- sample(
+    rownames(assay), 
+    size = nfeatures, 
+    replace = FALSE
+  )
+
+  all_features <- rownames(assay)
+  constant_features <- setdiff(all_features, variable_features)
+  
+  # Add a column to the assay's feature-level metadata indicating which
+  # features are variable.
+  is_variable <- all_features %in% variable_features
+  names(is_variable) <- all_features
+  variable_column <- paste("vf", method_name, layer_name, "variable", sep = "_")
+  assay[[variable_column]] <- is_variable
+  
+  # Add a column to the assay's feature-level metadata indicating the order
+  # (i.e. "rank") that each variable feature was selected in.
+  variable_rank <- seq_along(variable_features)
+  names(variable_rank) <- variable_features
+  constant_rank <- seq_along(constant_features) + length(variable_features)
+  names(constant_rank) <- constant_features
+  feature_rank <- c(variable_rank, constant_rank)
+  rank_column <- paste("vf", method_name, layer_name, "rank", sep = "_")
+  assay[[rank_column]] <- feature_rank
+
+  # Add a column to the assay's feature-level metadata containing a random
+  # values in reverse order from `variable_rank` (i.e. the lowest rank
+  # has the highest value).
+  feature_values <- runif(length(all_features))
+  feature_values <- sort(feature_values, decreasing = TRUE)
+  names(feature_values) <- names(feature_rank)
+  value_column <- paste("vf", method_name, layer_name, "value", sep = "_")
+  assay[[value_column]] <- feature_values
+  
+  return(assay)
+}
