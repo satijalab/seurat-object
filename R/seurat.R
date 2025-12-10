54fc93694abbafd05449feeaa296488a6cfdf4a3
Anagha Shenoy <90294056+anashen@users.noreply.github.com>
2025-12-10 13:38:24 -0500

    Update function comment for GetTissueCoordinates.Seurat

--- R/seurat.R
+++ R/seurat.R
@@ -1965,7 +1965,7 @@ GetImage.Seurat <- function(
 }
 
 #' @param image Name of \code{SpatialImage} object to get coordinates for; if
-#' \code{NULL}, will retrieve coordinates from the default image
+#' \code{NULL}, will retrieve coordinates for the default image
 #'
 #' @rdname GetTissueCoordinates
 #' @method GetTissueCoordinates Seurat
